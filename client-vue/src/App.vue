615360a2543b598b01ec004f985d4c11df701c7d
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2026-08-14 16:54:55 -0700

    Add auth-aware nav bar with logout

--- client-vue/src/App.vue
+++ client-vue/src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { useAuthStore } from './stores/auth'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+function handleLogout() {
+  authStore.logout()
+  router.push({ name: 'login' })
+}
 </script>
 
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
     <RouterLink to="/predictions">Predictions</RouterLink>
     <RouterLink to="/leaderboard">Leaderboard</RouterLink>
-    <RouterLink to="/signup">Sign Up</RouterLink>
+    
+    <template v-if="authStore.isLoggedIn">
+      <button @click="handleLogout">Log out</button>
+    </template>
+    <template v-else>
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/signup">Sign Up</RouterLink>
+    </template>
   </nav>
 
   <RouterView />
